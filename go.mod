4407c6196157c507182d5de844117b8c27dfe21f
louisaldorio <louisaldorio@gmail.com>
2021-01-05 11:56:10 +0700

    middleware auth

--- go.mod
+++ go.mod
@@ -4,5 +4,9 @@ go 1.15
 
 require (
 	github.com/99designs/gqlgen v0.13.0
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/stretchr/testify v1.6.1 // indirect
 	github.com/vektah/gqlparser/v2 v2.1.0
+	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
---
+++ middleware/auth.go
@@ -0,0 +1,74 @@
+package middleware
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+var userCtxKey = &contextKey{"user"}
+
+type contextKey struct {
+	name string
+}
+
+type UserClaim struct {
+	Username string
+	Role     string
+	jwt.StandardClaims
+}
+
+var jwtKey = []byte("secret")
+
+func Auth() func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			authToken := r.Header.Get("Authorization")
+
+			// Allow unauthenticated users in
+			if authToken == "" {
+				next.ServeHTTP(w, r)
+				return
+			}
+
+			//validate jwt token
+			jwtToken, err := ValidateToken(authToken)
+			if err != nil {
+				http.Error(w, "Invalid token", http.StatusForbidden)
+				return
+			}
+
+			//validate claim
+			claims, ok := jwtToken.Claims.(*UserClaim)
+			if !ok && !jwtToken.Valid {
+				http.Error(w, "Invalid token", http.StatusForbidden)
+				return
+			}
+
+			//return user data to req
+			ctx := context.WithValue(r.Context(), userCtxKey, claims)
+			reqWithCtx := r.WithContext(ctx)
+			next.ServeHTTP(w, reqWithCtx)
+		})
+	}
+}
+
+func ValidateToken(t string) (*jwt.Token, error) {
+	token, _ := jwt.ParseWithClaims(t, &UserClaim{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("There was an error")
+		}
+
+		return jwtKey, nil
+	})
+
+	return token, nil
+}
+
+// ForContext finds the user from the context. REQUIRES Middleware to have run.
+func AuthContext(ctx context.Context) *UserClaim {
+	raw, _ := ctx.Value(userCtxKey).(*UserClaim)
+	return raw
+}
